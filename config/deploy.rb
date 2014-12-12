f1a4329c3b4368566f72da0776be4ec1d5da0608
Kareem Amin <kareem@useframe.com>
2014-12-12 15:01:53 -0500

    updated deploy instructions

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,10 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
+      within release_path do
+        execute :rake, 'assets:precompile'
+        execute :rake, 'db:migrate'
+      end
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
