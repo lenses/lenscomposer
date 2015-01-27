34552053ca79b1b3bfb080b29e679284ef65fce0
Kareem Amin <kareem@useframe.com>
2015-01-27 12:02:09 -0500

    remove mongoid load not necessary anymore

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,6 @@ require 'action_mailer/railtie'
 require 'sprockets/railtie'
 require 'rails/test_unit/railtie'
 
-Mongoid.load!("config/mongoid.yml")
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
