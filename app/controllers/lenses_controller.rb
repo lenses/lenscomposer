fb635401b96c8cf913796e77268569e8f73e4593
Nisha Batra <nishabatra27@gmail.com>
2015-01-29 14:07:34 -0500

    saving, editing, and showing final result works, needs clean up

--- app/controllers/lenses_controller.rb
+++ app/controllers/lenses_controller.rb
@@ -7,27 +7,35 @@ class LensesController < ApplicationController
   end
 
   def create
-    lens_params = [:author, :name, :els]
+
+    lens_params = [:author, :name, :els, :final_result]
     attrb = select_params(lens_params)
     # Els is an array of Objects of Components Properties
     # that needs to be deserialized correctly
     attrb["els"] = JSON.parse(attrb["els"])
     lens = Lens.create(attrb)
+    debugger
     render json: lens.id.to_s.to_json
   end
 
   def show
     lens = Lens.find(params[:id])
-    idx = lens.els.find_index { |e| e["final_result"]}
-    if idx
-      final_result = Component.new(lens.els[idx])
-      @final_result_html = final_result.generate_html_tag
+    final_result = lens.final_result
+    if final_result
+      debugger
+      @final_result_html = lens.generate_html_tag(JSON.parse(final_result))
+    # idx = lens.els.find_index { |e| e["final_result"]}
+    # if idx
+    #   final_result = Component.new(lens.els[idx])
+    #   @final_result_html = final_result.generate_html_tag
     else
       render inline: "No final result for this lens"
     end
   end
 
   def edit
+    @lens = Lens.find(params[:id])
+    @els = @lens.els.to_json
   end
 
   def update
--- app/models/lens.rb
+++ app/models/lens.rb
@@ -3,5 +3,28 @@ class Lens
   include Mongoid::Attributes::Dynamic
 
   field :els, type: Array
+  field :final_result, type: Object
 
+  def generate_html_tag(element_obj)
+    current_state = JSON.parse(element_obj["componentState"])
+    attrs = generate_html_attrs(current_state)
+    tag_name = element_obj['componentName']
+    html_tag = "<#{tag_name} #{attrs}></#{tag_name}>"
+    return html_tag
+  end
+
+  def generate_html_attrs(attrs_obj)
+    attrs = ""
+    debugger
+    if attrs_obj
+      attrs_obj.each do |attr, value|
+          if value.kind_of?(Array)
+            attrs << "#{attr}= '#{value.to_json}' "
+          else
+            attrs << "#{attr}= \"#{value}\" "
+          end
+      end
+    end
+    return attrs
+  end
 end
